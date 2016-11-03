d7ecdd6f485c7e687bb684c815d0647a41cc1f46
IvoBiesdorf <ivobiesdorf@gmail.com>
2016-11-03 21:46:22 -0200

    Revisão 2 Adicionado a segunda linha do gráfico

--- src/NovoClass.java
+++ src/NovoClass.java
@@ -1,30 +1,46 @@
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class NovoClass {
 
     public static void main(String[] args) {
         DefaultCategoryDataset ds = new DefaultCategoryDataset();
-        ds.addValue(40.5, "maximo", "dia 1");
-        ds.addValue(38.2, "maximo", "dia 2");
-        ds.addValue(37.3, "maximo", "dia 3");
-        ds.addValue(31.5, "maximo", "dia 4");
-        ds.addValue(35.7, "maximo", "dia 5");
-        ds.addValue(42.5, "maximo", "dia 6");
+        ds.addValue(40.5, "Azul", "dia 1");
+        ds.addValue(18.2, "Azul", "dia 2");
+        ds.addValue(37.3, "Azul", "dia 3");
+        ds.addValue(21.5, "Azul", "dia 4");
+        ds.addValue(35.7, "Azul", "dia 5");
+        ds.addValue(48.5, "Azul", "dia 6");
+        ds.addValue(38.2, "Amarelo", "dia 1");
+        ds.addValue(37.3, "Amarelo", "dia 2");
+        ds.addValue(31.5, "Amarelo", "dia 3");
+        ds.addValue(35.7, "Amarelo", "dia 4");
+        ds.addValue(42.5, "Amarelo", "dia 5");
+        ds.addValue(46.5, "Amarelo", "dia 6");
 
 // cria o gráfico
-        JFreeChart grafico = ChartFactory.createLineChart("Meu Grafico", "Dia",
-                "Valor", ds, PlotOrientation.VERTICAL, true, true, false);
-
+        //JFrame jf = new JFrame();
+        JFreeChart grafico = ChartFactory.createLineChart("Teste de Gráfico","Dias","Valores", ds, PlotOrientation.VERTICAL, true, true,false );
+        CategoryItemRenderer renderer = grafico.getCategoryPlot().getRenderer();
+        renderer.setSeriesPaint( 0, Color.BLUE );
+        renderer.setSeriesPaint( 1, Color.YELLOW );
+        ChartPanel panel = new ChartPanel( grafico );
+//        jf.add( panel, BorderLayout.CENTER );
+//        jf.setVisible( true );
         OutputStream arquivo;
         try {
             arquivo = new FileOutputStream("grafico.png");
